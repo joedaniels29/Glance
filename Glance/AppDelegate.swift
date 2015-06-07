0006826725bb417b2bb232240fcb7b47fba5b06d
Joseph Daniels <jdanie37@jh.edu>
2015-06-07 14:30:17 -0400

    Initial Commit

---
+++ Glance/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Glance
+//
+//  Created by Joseph Daniels on 6/7/15.
+//  Copyright (c) 2015 Joseph Daniels. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Glance/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Glance
+//
+//  Created by Joseph Daniels on 6/7/15.
+//  Copyright (c) 2015 Joseph Daniels. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
